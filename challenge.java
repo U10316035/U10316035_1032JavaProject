36a9688b9610a41bb98cf871e084bd47d9cb3cc7
U10316035 <u10316035@go.utaipei.edu.tw>
2015-06-12 23:24:17 +0900

    Create challenge.java

---
+++ challenge.java
@@ -0,0 +1,686 @@
+package knowledge;
+
+public class challenge {
+	challenge(){
+		
+	}
+	String math(int random){
+		switch(random){
+		case 0:
+			return "1 + 1 = ?";
+		case 1:
+			return "2 + 2 = ?";
+		case 2:
+			return "3 + 3 = ?";
+		case 3:
+			return "4 + 4 = ?";
+		case 4:
+			return "5 + 5 = ?";
+		case 5:
+			return "6 + 6 = ?";
+		case 6:
+			return "7 + 7 = ?";
+		case 7:
+			return "8 + 8 = ?";
+		case 8:
+			return "9 + 9 = ?";
+		case 9:
+			return "10 + 10 = ?";
+			
+		}
+		return null;
+	}
+	
+	String game(int random){
+		switch(random){
+		case 0:
+			return "1 + 1 = ?";
+		case 1:
+			return "2 + 2 = ?";
+		case 2:
+			return "3 + 3 = ?";
+		case 3:
+			return "4 + 4 = ?";
+		case 4:
+			return "5 + 5 = ?";
+		case 5:
+			return "6 + 6 = ?";
+		case 6:
+			return "7 + 7 = ?";
+		case 7:
+			return "8 + 8 = ?";
+		case 8:
+			return "9 + 9 = ?";
+		case 9:
+			return "10 + 10 = ?";
+			
+		}
+		return null;
+	}
+	
+	String random(int random){
+		switch(random){
+		case 0:
+			return "1 + 1 = ?";
+		case 1:
+			return "2 + 2 = ?";
+		case 2:
+			return "3 + 3 = ?";
+		case 3:
+			return "4 + 4 = ?";
+		case 4:
+			return "5 + 5 = ?";
+		case 5:
+			return "6 + 6 = ?";
+		case 6:
+			return "7 + 7 = ?";
+		case 7:
+			return "8 + 8 = ?";
+		case 8:
+			return "9 + 9 = ?";
+		case 9:
+			return "10 + 10 = ?";
+			
+		}
+		return null;
+	}
+	
+	String other(int random){
+		switch(random){
+		case 0:
+			return "1 + 1 = ?";
+		case 1:
+			return "2 + 2 = ?";
+		case 2:
+			return "3 + 3 = ?";
+		case 3:
+			return "4 + 4 = ?";
+		case 4:
+			return "5 + 5 = ?";
+		case 5:
+			return "6 + 6 = ?";
+		case 6:
+			return "7 + 7 = ?";
+		case 7:
+			return "8 + 8 = ?";
+		case 8:
+			return "9 + 9 = ?";
+		case 9:
+			return "10 + 10 = ?";
+			
+		}
+		return null;
+	}
+	
+	String mathAns(int random, int answer){
+		switch(random){
+		case 0:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "4";
+			}
+		case 1:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "4";
+			}
+		case 2:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "6";
+			}
+		case 3:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "8";
+			}
+		case 4:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "10";
+			}
+		case 5:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "12";
+			}
+		case 6:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "14";
+			}
+		case 7:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "16";
+			}
+		case 8:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "18";
+			}
+		case 9:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "20";
+			}
+		}
+		return null;
+	}
+	
+	String gameAns(int random, int answer){
+		switch(random){
+		case 0:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "4";
+			}
+		case 1:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "4";
+			}
+		case 2:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "6";
+			}
+		case 3:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "8";
+			}
+		case 4:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "10";
+			}
+		case 5:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "12";
+			}
+		case 6:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "14";
+			}
+		case 7:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "16";
+			}
+		case 8:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "18";
+			}
+		case 9:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "20";
+			}
+		}
+		return null;
+	}
+	
+	String otherAns(int random, int answer){
+		switch(random){
+		case 0:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "4";
+			}
+		case 1:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "4";
+			}
+		case 2:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "6";
+			}
+		case 3:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "8";
+			}
+		case 4:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "10";
+			}
+		case 5:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "12";
+			}
+		case 6:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "14";
+			}
+		case 7:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "16";
+			}
+		case 8:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "18";
+			}
+		case 9:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "20";
+			}
+		}
+		return null;
+	}
+	
+	String randomAns(int random, int answer){
+		switch(random){
+		case 0:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "4";
+			}
+		case 1:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "4";
+			}
+		case 2:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "6";
+			}
+		case 3:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "8";
+			}
+		case 4:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "10";
+			}
+		case 5:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "12";
+			}
+		case 6:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "14";
+			}
+		case 7:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "16";
+			}
+		case 8:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "18";
+			}
+		case 9:
+			switch(answer){
+			case 0:
+				return "1";
+			case 1:
+				return "2";
+			case 2:
+				return "3";
+			case 3:
+				return "20";
+			}
+		}
+		return null;
+	}
+	
+	String ansToMath(int random){
+		switch(random){
+		case 0:
+			return "2";
+		case 1:
+			return "4";
+		case 2:
+			return "6";
+		case 3:
+			return "8";
+		case 4:
+			return "10";
+		case 5:
+			return "12";
+		case 6:
+			return "14";
+		case 7:
+			return "16";
+		case 8:
+			return "18";
+		case 9:
+			return "20";
+			
+		}
+		return null;
+	}
+	
+	String ansToGame(int random){
+		switch(random){
+		case 0:
+			return "2";
+		case 1:
+			return "4";
+		case 2:
+			return "6";
+		case 3:
+			return "8";
+		case 4:
+			return "10";
+		case 5:
+			return "12";
+		case 6:
+			return "14";
+		case 7:
+			return "16";
+		case 8:
+			return "18";
+		case 9:
+			return "20";
+			
+		}
+		return null;
+	}
+	
+	String ansToOther(int random){
+		switch(random){
+		case 0:
+			return "2";
+		case 1:
+			return "4";
+		case 2:
+			return "6";
+		case 3:
+			return "8";
+		case 4:
+			return "10";
+		case 5:
+			return "12";
+		case 6:
+			return "14";
+		case 7:
+			return "16";
+		case 8:
+			return "18";
+		case 9:
+			return "20";
+			
+		}
+		return null;
+	}
+	
+	String ansToRandom(int random){
+		switch(random){
+		case 0:
+			return "2";
+		case 1:
+			return "4";
+		case 2:
+			return "6";
+		case 3:
+			return "8";
+		case 4:
+			return "10";
+		case 5:
+			return "12";
+		case 6:
+			return "14";
+		case 7:
+			return "16";
+		case 8:
+			return "18";
+		case 9:
+			return "20";
+			
+		}
+		return null;
+	}
+}
